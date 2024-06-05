f2760cb6b0e180b4c1be0c0759fbd2593ee7bb46
Gairaud Alexis <a.gairaud@catie.fr>
2024-06-05 12:10:22 +0200

    Exemple of FRS read

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ namespace {
 static DigitalOut led1(LED1);
 static InterruptIn wake_up(WKUP);
 bool data_available = false;
+bool state = true;
 
 void flip()
 {
@@ -27,15 +28,26 @@ int main()
     I2C i2c(I2C1_SDA, I2C1_SCL);
     BNO085 bno085(&i2c, 0x4B);
     bno085.initialize();
-    bno085.enable_step_counter(500);
     ThisThread::sleep_for(1000ms);
-    while (true) {
-        if (data_available) {
-            data_available = false;
-            bno085.get_readings();
-            uint16_t steps = bno085.get_step_count();
-            printf("Steps : %u\n", steps);
-            ThisThread::sleep_for(250ms);
+
+    printf("========== For accelerometer ==========\n");
+    float get_acce_range = bno085.get_range(FRS_RECORDID_ROTATION_VECTOR);
+    printf("Range : \t%f\n", get_acce_range);
+    float get_acce_resol = bno085.get_resolution(FRS_RECORDID_ROTATION_VECTOR);
+    printf("Resolution : \t%f\n", get_acce_resol);
+    int16_t get_acce_q_1 = bno085.get_q_1(FRS_RECORDID_ROTATION_VECTOR);
+    printf("Q1 : \t%d\n", get_acce_q_1);
+    int16_t get_acce_q_2 = bno085.get_q_2(FRS_RECORDID_ROTATION_VECTOR);
+    printf("Q2 : \t%d\n", get_acce_q_2);
+    int16_t get_acce_q_3 = bno085.get_q_3(FRS_RECORDID_ROTATION_VECTOR);
+    printf("Q3 : \t%d\n", get_acce_q_3);
+    printf("=======================================\n\n\n");
+    while (state) {
+        uint32_t rot_vector_power = bno085.read_frs_word(FRS_RECORDID_ROTATION_VECTOR, 3) & 0xFFFF;
+        float rot_power = bno085.q_to_float(rot_vector_power, 10);
+        printf("Rotation vector power : %f mA \n", rot_power);
+        if (rot_power > 0) {
+            state = false;
         }
     }
 }
